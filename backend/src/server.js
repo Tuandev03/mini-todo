a4da63b3bf4b36f7bc6065a1d4f1bbaa5cc4ce80
Tuan <thanhtuan.mail.dev@gmail.com>
2025-11-29 13:01:45 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,36 @@
 import express from "express";
-import taskRoute from "./routes/taskRoutes.js";
+import taskRoute from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import cors from "cors"
-import path from "path"
+import cors from "cors";
+import path from "path";
+
 dotenv.config();
+
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
+
 const app = express();
+
 // middlewares
 app.use(express.json());
-if(process.env.NODE_ENV !== "production"){
-  app.use(cors({origin: "http://localhost:5173"}))
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
 }
+
 app.use("/api/tasks", taskRoute);
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
-app.get("*",(req,res)=> {
-  res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
-})
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
+
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`server start: ${PORT}`);
+    console.log(`server bắt đầu trên cổng ${PORT}`);
   });
 });
